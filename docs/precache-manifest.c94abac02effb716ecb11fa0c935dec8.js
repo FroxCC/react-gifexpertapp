de4c2b8ecbfbc89e8ad46782bcc77b610e4c3c13
Carlos Crisner <mrfrox259@gmail.com>
2021-10-08 12:36:05 -0400

    Docs added

---
+++ docs/precache-manifest.c94abac02effb716ecb11fa0c935dec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7749ccf2636736b2a6ed2e5b069d969f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96fd29c4b253e9b184a4",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fd29c4b253e9b184a4",
+    "url": "/static/js/main.24321d3d.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
